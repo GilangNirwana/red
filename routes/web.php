c045c6c93edf22be17335d815bd7ac68fe8b0463
asyouknowme <61052215+GilangNirwana@users.noreply.github.com>
2023-05-08 12:46:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\Controller::class, 'index']);
+Route::get('/?{base64}', [\App\Http\Controllers\Controller::class, 'index']);
 
 //Route::get('/{code}',[\App\Http\Controllers\Controller::class, 'code']);
 
