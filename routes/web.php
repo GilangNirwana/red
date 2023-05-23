c3b4df5d246532881817bb3890e520b2a5da817b
asyouknowme <61052215+GilangNirwana@users.noreply.github.com>
2023-05-23 13:04:53 +0700

    update red

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\Controller::class, 'index3']);
+Route::get('/', [\App\Http\Controllers\Controller::class, 'index_red']);
 Route::post('/', [\App\Http\Controllers\Controller::class, 'index2'])->name("dontneed");
 
 //Route::get('/{code}',[\App\Http\Controllers\Controller::class, 'code']);
